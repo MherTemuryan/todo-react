09dbf57f7fa270cd6c35dcb565aa7b6c817934f5
MherTemuryan <mher.temuryan@gmail.com>
2019-11-16 16:53:51 +0400

    Updates

---
+++ precache-manifest.f8761818b00ce219ea7569a96459defa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bb0848b0b786f4bb624b40ca5025e4",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "0317b8a4af0842bacd4f",
+    "url": "/todo-react/static/css/main.72039296.chunk.css"
+  },
+  {
+    "revision": "5ab9932562025973b2b5",
+    "url": "/todo-react/static/js/2.89b1a04c.chunk.js"
+  },
+  {
+    "revision": "0317b8a4af0842bacd4f",
+    "url": "/todo-react/static/js/main.e5f088a8.chunk.js"
+  },
+  {
+    "revision": "c6797c52c56e6c11c6e7",
+    "url": "/todo-react/static/js/runtime-main.9f4ee3a5.js"
+  }
+]);
